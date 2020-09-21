10980c86b49ad0efea279e29e6dc6b651c8a16a6
William Hale <wmhale92@gmail.com>
2020-09-21 02:36:11 -0600

    Added guildMemberAdd event listener and action.

--- index.js
+++ index.js
@@ -76,5 +76,12 @@ client.on('message', message => {
 	}
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+	const welcomeChannel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+	if (!channel) return;
+	welcomeChannel.send(`Welcome to the server, ${member}, please make yourself at home and use the !help command if you want to know more about what I can do.` );
+  });
+
 //Bot log-in
 client.login(process.env.TOKEN);
