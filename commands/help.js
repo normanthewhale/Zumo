ce7bb7bb88f6d048482662e713aa848f0f22a446
William Hale <wmhale92@gmail.com>
2020-11-14 01:17:24 -0700

    Minor enhancements

--- commands/help.js
+++ commands/help.js
@@ -15,8 +15,8 @@ module.exports = {
         //Response if specific command isn't designated.
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join('\n'));
-            data.push(`\nYou can send \`${PREFIX}help [command name]\` to get info on a specific command!`);
+            data.push(commands.map(command => command.name).join(', '));
+            data.push(`\nYou can send \`${PREFIX}help <command name>\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })
                 .then(() => {
