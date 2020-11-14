6c102ce551efa6db0234759c0107c81fa08537c5
William Hale <wmhale92@gmail.com>
2020-11-14 01:02:20 -0700

    Command list in 'help' has line breaks

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,7 @@ module.exports = {
         //Response if specific command isn't designated.
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
+            data.push(commands.map(command => command.name).join('\n'));
             data.push(`\nYou can send \`${PREFIX}help [command name]\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })
