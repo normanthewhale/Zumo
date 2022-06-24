0e67db7ac0751c117e8ed5d7d607e4bd5ade566e
William Hale <wmhale92@gmail.com>
2022-06-24 12:32:12 -0600

    dalle command v1.1

--- commands/dalle.js
+++ commands/dalle.js
@@ -8,6 +8,23 @@ module.exports = {
 	cooldown: 0,
 	execute(message, args) {
 		let prompt = args.join(' ');
-		message.channel.send('The prompt is: ' + prompt);
+        message.channel.send('The prompt is: ' + prompt);
+        let requestURL = 'https://backend.craiyon.com/generate';
+        let request = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: '{"prompt": "'+ prompt +'"}'
+        }
+        fetch(requestURL, request).then(response => {
+            response.json();
+        }).then(data => {
+            console.log(data);
+            message.channel.send('The response was successful!');
+        }).catch(error =>{
+            console.error(error);
+            message.channel.send('There was an error.');
+        });
 	},
 };
