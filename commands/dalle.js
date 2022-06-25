b489313793564ed4c68af4290e1c8d9f4e84a363
William Hale <wmhale92@gmail.com>
2022-06-24 23:04:40 -0600

    testing fetch

--- commands/dalle.js
+++ commands/dalle.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 module.exports = {
 	name: 'dalle',
 	aliases: [],
@@ -9,22 +11,26 @@ module.exports = {
 	execute(message, args) {
 		let prompt = args.join(' ');
         message.channel.send('The prompt is: ' + prompt);
-        let requestURL = 'https://backend.craiyon.com/generate';
-        let request = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: '{"prompt": "'+ prompt +'"}'
-        }
-        fetch(requestURL, request).then(response => {
-            response.json();
-        }).then(data => {
-            console.log(data);
-            message.channel.send('The response was successful!');
-        }).catch(error =>{
-            console.error(error);
-            message.channel.send('There was an error.');
-        });
+        const response = await fetch('https://api.github.com/users/github');
+        const data = await response.json();
+
+        console.log(data);
+        // let requestURL = 'https://backend.craiyon.com/generate';
+        // let request = {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: '{"prompt": "'+ prompt +'"}'
+        // }
+        // fetch(requestURL, request).then(response => {
+        //     response.json();
+        // }).then(data => {
+        //     console.log(data);
+        //     message.channel.send('The response was successful!');
+        // }).catch(error =>{
+        //     console.error(error);
+        //     message.channel.send('There was an error.');
+        // });
 	},
 };
